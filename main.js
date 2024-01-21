194170c1bbc03a28e4a9ee3bcaed71e170b7b5a6
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2024-01-21 18:37:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,45 @@
 "use strict";
 const apiUrl = "/db.json";
-const hello = "greeting";
-console.log(hello);
 
 async function fetchData() {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
-    return data;
+
+    const beers = Object.values(data);
+    console.log(beers);
+
+    return beers;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
 
-(function displayBeerNames(beers) {
-  const beerList = document.getElementById("#beerList");
+function displayBeerNames(beers) {
+  const beerList = document.getElementById("beerList");
 
-  if (Array.isArray(beers) && beers.length > 0) {
-    beers.forEach((beer) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = beer.name;
+  beers.forEach((beer) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = beer.id;
 
-      listItem.addEventListener("click", () => {
-        displayRandomImage(beers);
-      });
-
-      beerList.appendChild(listItem);
+    listItem.addEventListener("click", () => {
+      displayRandomImage(data);
     });
 
-    // Clear previous content and append the new list to the nav
-    beerNav.innerHTML = "";
     beerList.appendChild(listItem);
-  } else {
-    // console.error("Invalid data format or empty array for beers.");
-  }
-})(async () => {
-  const data = await fetchData();
-  console.log(data);
-  //   const beers = Object.values(data);
-  //   console.log(beers);
+  });
 
+  // beerList.appendChild(listItem);
+}
+(async () => {
+  const data = await fetchData();
+  const beers = Object.values(data);
+
+  console.log(data);
+
+  //   console.log(beers);
   displayBeerNames(beers);
 
   console.log(data);
-});
+})();
